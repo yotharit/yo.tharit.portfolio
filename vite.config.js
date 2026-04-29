fafa3175fe0e32cc11f1a76d40224762b6f44884
Tharit Pongsaneh <yo.tharit@bitkub.com>
2026-04-30 04:15:50 +0700

    Config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/yo.tharit.portfolio/', // <-- ADD THIS LINE (must match your repo name exactly)
 })
